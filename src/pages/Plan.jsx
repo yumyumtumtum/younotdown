a4da0d85d43a1da0414792416fb6699c5bb89234
Turok <timothyturok@Turoks-MacBook-Pro.local>
2024-11-26 23:32:21 -0500

    fix:tailwind

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Plan=() => {
     return(
-        <div className="text-9xl">
-            Hello, World!
+        <div className="text-4xl font-bold">
+         Hello, World!
         </div>
     )
 }
