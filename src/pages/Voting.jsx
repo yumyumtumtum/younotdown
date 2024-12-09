a59ec86012ffe4bab73c416023d1b0c57fa7b830
David Kwon <davidkwonh@gmail.com>
2024-12-09 13:57:41 -0800

    fixing styling for voting buttons

--- src/pages/Voting.jsx
+++ src/pages/Voting.jsx
@@ -5,22 +5,27 @@ import Button from '../components/Button/Button'
 /**
  * Page: YouDown
  * Description: This is the page where people choose if they are down or not
-/** ****  01d50298-3648-4dd2-b691-cfd4efcc1fbb  ****** */
+ *
+ *
+ * TO DO:
+ * 1. Create an interaction and logic for conditionals/dependents
+ * 2. Link this part has access to the people from CreatePoll
+=/** ****  01d50298-3648-4dd2-b691-cfd4efcc1fbb  ****** */
 function YouDown() {
   return (
     <div>
       <Header />
       <div className="bg-app-background bg-cover bg-center min-h-screen text-center text-4xl font-bold">
         Are you down?
-        <div className="h-56 grid grid-cols-3 gap-4 content-center ...">
+        <div className="h-56 grid grid-cols-3 gap-4 content-center justify-center px-4">
           <div>
-            <Button bg="black" btnText="nah" textColor="white" />
+            <Button bg="black" btnText="No" textColor="white" />
           </div>
           <div>
-            <Button bg="bg-green-500" btnText="maybe" textColor="white" />
+            <Button bg="bg-green-500" btnText="Maybe" textColor="white" />
           </div>
           <div>
-            <Button bg="bgred-500" btnText="bet" textColor="white" />
+            <Button bg="bgred-500" btnText="YES" textColor="white" />
           </div>
         </div>
       </div>
