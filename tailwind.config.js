4b08ef790820288bf5718be1c4a3163dfc1e5c8c
David Kwon <davidkwonh@gmail.com>
2024-12-09 14:08:17 -0800

    using prettier to fix spacing and adding colors for the voting options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
         'app-bg': "url('/younotdown.webp')",
       },
       colors: {
-          
-        'black': '#000000',
+        red: '#FF0000',
+        green: '#00FF00',
+        black: '#000000',
       },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
 }
