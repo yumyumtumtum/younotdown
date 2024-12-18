6d9e66b1f67f5bd8644d4bbe390a960b5f046381
Turok <timothyturok@Turoks-MacBook-Pro.local>
2024-12-18 00:18:55 -0500

    add landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 function LandingPage() {
   return (
     <div className="bg-cyan-100 flex h-screen w-full items-center justify-center">
-      <div className="border-solid border-2 border-cyan-950 p-96 rounded-3xl">
-        <h1 className=""> You not down </h1>
-        <Button>Plan </Button>
+      <div className="border-solid border-2 border-cyan-950 p-96 rounded-3xl flex-col center-center gap-10">
+        <h1> You not down </h1>
+
+        <Button large> Spontaneous Plan </Button>
       </div>
     </div>
   )
