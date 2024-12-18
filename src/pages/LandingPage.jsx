13da90261829dfd99439f99919b52f9e9b493f76
Turok <timothyturok@Turoks-MacBook-Pro.local>
2024-12-18 00:23:13 -0500

    add router link

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
+
 function LandingPage() {
   return (
     <div className="bg-cyan-100 flex h-screen w-full items-center justify-center">
       <div className="border-solid border-2 border-cyan-950 p-96 rounded-3xl flex-col center-center gap-10">
         <h1> You not down </h1>
 
-        <Button large> Spontaneous Plan </Button>
+        <Link to="/home">
+          <Button large> Start a Poll </Button>
+        </Link>
       </div>
     </div>
   )
