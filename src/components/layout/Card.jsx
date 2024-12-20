6733a1e5f61743f3993282037c4a7e087e2712ba
jchoi4497 <jchoi4497@gmail.com>
2024-12-19 16:33:06 -0800

    Add Grid and Table to Components to organized page

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,7 +1,7 @@
 function Card({ children, label }) {
   return (
     <div className="py-2 px-4 border-solid border-2 border-gray-300 rounded-md flex flex-col gap-y-2 shadow-lg">
-      <h4 className="font-bold py-2 px-4 bg-sky-200 w-fit rounded-xl">{label}</h4>
+      <h4 className="font-bold py-4 px-4 mb-6 bg-sky-200 w-fit rounded-xl">{label}</h4>
       {children}
     </div>
   )
--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -4,45 +4,49 @@ function Components() {
       <div className="text-xl font-bold">Components</div>
 
       <Card label="Base Elements">
-        <h1> Header 1 </h1>
-        <h2> Header 2 </h2>
-        <h3> Header 3 </h3>
-        <h4> Header 4 </h4>
-        <h5> Header 5 </h5>
-        <h6> Header 6 </h6>
+        <div className='grid gap-10 grid-cols-3 grid-rows-3'>
+          <h1> Header 1 </h1>
+          <h2> Header 2 </h2>
+          <h3> Header 3 </h3>
+          <h4> Header 4 </h4>
+          <h5> Header 5 </h5>
+          <h6> Header 6 </h6>
+        </div>
       </Card>
 
       <Card label="Buttons">
-        <div className="text-lg ">Buttons</div>
-
-        <Button small>Small Button</Button>
-        <Button>Regular Button</Button>
-        <Button medium>Regular Button</Button>
-
-        <Button secondary small>
-          Small Secondary Button
-        </Button>
-        <Button secondary>Regular Secondary Button</Button>
-        <Button secondary medium>
-          Regular Secondary Button
-        </Button>
-
-        <Button tertiary small>
-          Small tertiary Button
-        </Button>
-        <Button tertiary>Regular tertiary Button</Button>
-        <Button tertiary medium>
-          Regular tertiary Button
-        </Button>
-
-        <Button success>Success Button</Button>
-        <Button danger>Danger Button</Button>
-        <Button warning>Warning Button</Button>
-        <Button info>Info Button</Button>
-
-        <Button primary disabled>
-          Info Button
-        </Button>
+        <table className="table-fixed mb-20 border-separate border-spacing-y-3">
+          <thead>
+            <tr>
+              <th className='text-left'>Type</th>
+              <th className='text-left'>Small</th>
+              <th className='text-left'>Medium</th>
+              <th className='text-left'>Large</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Primary</td>
+              <td><Button small>Small Button</Button></td>
+              <td><Button>Regular Button</Button></td>
+              <td><Button medium>Regular Button</Button></td>
+            </tr>
+            <tr>
+              <td>Secondary</td>
+              <td><Button secondary small>Small Secondary Button</Button></td>
+              <td><Button secondary>Regular Secondary Button</Button></td>
+              <td><Button secondary medium>Regular Secondary Button</Button></td>
+            </tr>
+            <tr>
+              <td>Miscellaneous</td>
+              <td><Button success>Success Button</Button></td>
+              <td><Button danger>Danger Button</Button></td>
+              <td><Button warning>Warning Button</Button></td>
+              <td><Button info>Info Button</Button></td>
+              <td><Button primary disabled>Info Button</Button></td>
+            </tr>
+          </tbody>
+        </table>
       </Card>
     </div>
   )
