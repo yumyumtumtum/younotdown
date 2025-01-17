2a3e29aaef6fe68c826a20bf8b90cdc0c7a90db5
Turok <timothyturok@Turoks-MacBook-Pro.local>
2025-01-16 20:12:39 -0500

    going somehwere

--- src/pages/Poll.jsx
+++ src/pages/Poll.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { useParams } from 'react-router-dom'
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import db from '../firebase' // Assuming your Firestore instance is in a separate file
+
+import _map from 'lodash/map'
 
 function PollDetails() {
   const { docId } = useParams()
@@ -9,11 +11,33 @@ function PollDetails() {
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(null)
 
+  const pollName = useMemo(() => {
+    return pollData?.name
+  })
+  const pollParticipants = useMemo(() => {
+    return _map(pollData?.participants, ({ name, answer }, index) => {
+      return (
+        <div key={`${index}-participant`}>
+          <div>
+            <strong>name:</strong>
+            {name}
+          </div>
+          <div>
+            <strong>answer:</strong>
+            {answer}
+          </div>
+        </div>
+      )
+    })
+  })
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const docRef = doc(db, 'poll', docId)
         const docSnap = await getDoc(docRef)
+
+        console.log(docSnap.data())
 
         if (docSnap.exists()) {
           setPollData(docSnap.data())
@@ -30,30 +54,29 @@ function PollDetails() {
     fetchData()
   }, [docId]) // Only re-fetch data when docId changes
 
+  const changePollData = () => {
+    const docRef = doc(db, 'poll', docId)
+
+    updateDoc(docRef, {})
+  }
+
+  // Poll States
   if (isLoading) {
     return <div>Loading...</div>
   }
-
   if (error) {
     return <div>Error: {error}</div>
   }
-
   if (!pollData) {
     return <div>No poll data found.</div>
   }
-
   return (
-    <div>
-      <h2>Poll Details</h2>
-      <ul>
-        {Object.keys(pollData)
-          .filter((key) => key.startsWith('name'))
-          .map((key, index) => (
-            <li key={index}>
-              {key}: {pollData[key]}
-            </li>
-          ))}
-      </ul>
+    <div className="flex flex-col h-screen w-full items-center mt-40 gap-8">
+      <h2>{pollName} Details</h2>
+
+      {pollParticipants}
+
+      <Button onClick={changePollData}>update doc</Button>
     </div>
   )
 }
